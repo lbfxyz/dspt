29102dcdc21491f6510e80f4783851a93abe9112
lxyz <bf_liu@outlook.com>
2020-12-09 14:46:19 +0800

    init

---
+++ src/main/java/xyz/lbf/dspt/DsptApplication.java
@@ -0,0 +1,13 @@
+package xyz.lbf.dspt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/lbf/dspt/DsptApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lbf.dspt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
